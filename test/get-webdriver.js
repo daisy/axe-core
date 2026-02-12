eb04d001bfe0cfec3938badd16acd152873e0ccf
Daniel Weck <daniel.weck@gmail.com>
2026-02-12 08:20:56 +0000

    fix: fallback to CHROME_BIN in ChromeDriver hookup

--- test/get-webdriver.js
+++ test/get-webdriver.js
@@ -7,9 +7,10 @@ require('dotenv').config({ path: '~/.browser-driver-manager/.env' });
 const chromedriverPath =
   process.env.CHROMEDRIVER_BIN ||
   process.env.CHROMEDRIVER_TEST_PATH ||
-  chromedriver.path;
+  chromedriver.path ||
+  process.env.CHROME_BIN;
 console.log(
-  `CHROME DRIVER (get) === ${chromedriverPath} (${process.env.CHROMEDRIVER_BIN} / ${process.env.CHROMEDRIVER_TEST_PATH} / ${chromedriver.path}) [${process.env.CHROME_TEST_VERSION}] ** ${process.env.CHROME_TEST_PATH}`
+  `CHROME DRIVER (get) === ${chromedriverPath} (${process.env.CHROMEDRIVER_BIN} / ${process.env.CHROMEDRIVER_TEST_PATH} / ${chromedriver.path / ${process.env.CHROME_BIN}}) [${process.env.CHROME_TEST_VERSION}] ** ${process.env.CHROME_TEST_PATH}`
 );
 
 const getWebdriver = () => {
